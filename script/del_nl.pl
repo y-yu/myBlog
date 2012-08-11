80d9284674d4efc8e62bb5b4bff6db24b82e4ca1
yoshimuraYuu <yoshiyuu0310@gmail.com>
2012-08-11 17:08:39 +0900

    add article: delete_newline

---
+++ script/del_nl.pl
@@ -0,0 +1,34 @@
+use 5.14.0;
+
+open my $fh, "<", $ARGV[0];
+my @lines = <$fh>;
+close $fh;
+
+my $src = 0;
+my $res = "";
+
+for (@lines) {
+	if ($src) {
+		if (m/^```/) {
+			$src = 0;
+		}
+		$res .= $_;
+	} 
+	elsif (m/^\s*\* .*[^*]$/ || m/^(#|\d\. | {4}|>)/) {
+		$res .= $_;
+	}
+	elsif ($_ eq "\n") {
+		$res .= "\n$_";
+	}
+	elsif (m/^```/) {
+		$res .= $_;
+		$src++;
+	}
+	else {
+		$res .= (s/\n$//r);
+	}
+}
+
+open $fh, ">", $ARGV[1];
+print $fh $res;
+close $fh;
---
+++ script/preview.pl
@@ -0,0 +1,20 @@
+use 5.14.0;
+
+my $result = `wget -O - --no-check-certificate --header="Content-Type: text/plain" --post-file="$ARGV[0]" https://api.github.com/markdown/raw`;
+
+open my $fh, ">", $ARGV[1];
+
+say $fh "<html>\n<head>";
+say $fh '<link href="./css/reset.css" rel="stylesheet" type="text/css" />';
+say $fh '<link href="./css/960.css" rel="stylesheet" type="text/css" />';
+say $fh '<link href="./css/uv_active4d.css" rel="stylesheet" type="text/css" />';
+say $fh '<link href="./css/documentation.css" media="screen" rel="stylesheet" type="text/css">';
+say $fh '<link href="./css/pygments.css" media="screen" rel="stylesheet" type="text/css">';
+say $fh '<link href="./css/documentation.css" media="screen" rel="stylesheet" type="text/css">';
+say $fh '</head><body>';
+
+say $fh $result;
+
+say $fh '</body></html>';
+
+close $fh;
